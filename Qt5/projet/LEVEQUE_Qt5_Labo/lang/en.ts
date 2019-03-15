dd86f35b8849e23e96ab6a2e8af521fab2fbc2bb
dorianleveque <dorian.leveque@outlook.fr>
2019-03-15 14:11:54 +0100

    add language options

---
+++ Qt5/projet/LEVEQUE_Qt5_Labo/lang/en.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_150" sourcelanguage="en_150">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="33"/>
+        <source>New</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="38"/>
+        <source>Open</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="43"/>
+        <source>Save</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="48"/>
+        <source>Save as</source>
+        <translation>Save as</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="53"/>
+        <source>Exit</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="61"/>
+        <source>&amp;Line</source>
+        <translation>&amp;Line</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="67"/>
+        <source>&amp;Rectangle</source>
+        <translation>&amp;Rectangle</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="72"/>
+        <source>&amp;Ellipse</source>
+        <translation>&amp;Ellipse</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="77"/>
+        <source>&amp;Polygon</source>
+        <translation>&amp;Polygon</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="82"/>
+        <source>&amp;Text</source>
+        <translation>&amp;Text</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="91"/>
+        <source>Color</source>
+        <translation>Color</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="88"/>
+        <source>Line</source>
+        <translation>Line</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="89"/>
+        <source>Width</source>
+        <translation>Width</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="92"/>
+        <source>Fill</source>
+        <translation>Fill</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="94"/>
+        <source>Font</source>
+        <translation>Font</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="97"/>
+        <source>About Us</source>
+        <translation>About Us</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="257"/>
+        <source>About Qt</source>
+        <translation>About Qt</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="99"/>
+        <source>About this Application</source>
+        <translation>About this Application</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="106"/>
+        <source>&amp;File</source>
+        <translation>&amp;File</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="114"/>
+        <source>&amp;Tools</source>
+        <translation>&amp;Tools</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="122"/>
+        <source>&amp;Style</source>
+        <translation>&amp;Style</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="123"/>
+        <source>Pen</source>
+        <translation>Pen</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="128"/>
+        <source>Brush</source>
+        <translation>Brush</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="135"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Help</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="200"/>
+        <source>Quit ?</source>
+        <translation>Quit ?</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="200"/>
+        <source>Do you want to quit without saving ?</source>
+        <translation>Do you want to quit without saving ?</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="254"/>
+        <source>About Me</source>
+        <translation>About Me</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="254"/>
+        <source>Application created by LEVEQUE Dorian
+copyright Â© LEVEQUE Dorian 2019</source>
+        <translation>Application created by LEVEQUE Dorian
+copyright Â© LEVEQUE Dorian 2019</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="257"/>
+        <source>This application is developed with PyQt5.
+For more information, please visit
+the following site https://www.qt.io
+
+Version de PyQt5: 5.12</source>
+        <translation>This application is developed with PyQt5.
+For more information, please visit
+the following site https://www.qt.io
+
+Version de PyQt5: 5.12</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="260"/>
+        <source>About App</source>
+        <translation>About App</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="260"/>
+        <source>Version Alpha 0.1</source>
+        <translation>Version Alpha 0.1</translation>
+    </message>
+</context>
+</TS>
---
+++ Qt5/projet/LEVEQUE_Qt5_Labo/lang/fr.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_150">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="33"/>
+        <source>New</source>
+        <translation>Nouveau</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="38"/>
+        <source>Open</source>
+        <translation>Ouvrir</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="43"/>
+        <source>Save</source>
+        <translation>Sauvegarder</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="48"/>
+        <source>Save as</source>
+        <translation>Sauvegarder sous...</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="53"/>
+        <source>Exit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="61"/>
+        <source>&amp;Line</source>
+        <translation>&amp;Ligne</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="67"/>
+        <source>&amp;Rectangle</source>
+        <translation>&amp;Rectangle</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="72"/>
+        <source>&amp;Ellipse</source>
+        <translation>&amp;Ellipse</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="77"/>
+        <source>&amp;Polygon</source>
+        <translation>&amp;Polygone</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="82"/>
+        <source>&amp;Text</source>
+        <translation>&amp;Texte</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="91"/>
+        <source>Color</source>
+        <translation>Couleur</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="88"/>
+        <source>Line</source>
+        <translation>Line</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="89"/>
+        <source>Width</source>
+        <translation>Longueur</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="92"/>
+        <source>Fill</source>
+        <translation>Remplissage</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="94"/>
+        <source>Font</source>
+        <translation>Police</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="97"/>
+        <source>About Us</source>
+        <translation>A propos de nous</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="257"/>
+        <source>About Qt</source>
+        <translation>A propos de Qt</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="99"/>
+        <source>About this Application</source>
+        <translation>A propos de l&apos;application</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="106"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="114"/>
+        <source>&amp;Tools</source>
+        <translation>&amp;Outils</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="122"/>
+        <source>&amp;Style</source>
+        <translation>&amp;Style</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="123"/>
+        <source>Pen</source>
+        <translation>Crayon</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="128"/>
+        <source>Brush</source>
+        <translation>Pinceau</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="135"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="200"/>
+        <source>Quit ?</source>
+        <translation>Quitter ?</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="200"/>
+        <source>Do you want to quit without saving ?</source>
+        <translation>Voulez-vous quitter sans sauvegarder ?</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="254"/>
+        <source>About Me</source>
+        <translation>A propos de moi</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="254"/>
+        <source>Application created by LEVEQUE Dorian
+copyright Â© LEVEQUE Dorian 2019</source>
+        <translation>Application créée par LEVEQUE Dorian
+copyright Â© LEVEQUE Dorian 2019</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="257"/>
+        <source>This application is developed with PyQt5.
+For more information, please visit
+the following site https://www.qt.io
+
+Version de PyQt5: 5.12</source>
+        <translation>Cette application a été développé avec PyQt5
+Pour plus d&apos;information, veuillez visiter
+le site suivant : https://www.qt.io
+
+Version de PyQt5: 5.12</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="260"/>
+        <source>About App</source>
+        <translation>A propos de l&apos;application</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../../../../Scolarit?/ENIB/Cours/S7/CAI/Qt5/projet/LEVEQUE_Qt5_Labo/mainwindow.py" line="260"/>
+        <source>Version Alpha 0.1</source>
+        <translation>Version Alpha 0.1</translation>
+    </message>
+</context>
+</TS>
