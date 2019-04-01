74f9acc3c1cc46155845622a8a999507e36fffa6
dorianleveque <dorian.leveque@outlook.fr>
2019-04-01 19:57:44 +0200

    Version 1.1

--- Qt5/projet/LEVEQUE_Qt5_Labo/settings.py
+++ Qt5/projet/LEVEQUE_Qt5_Labo/settings.py
@@ -5,9 +5,17 @@ class Settings():
     def __init__(self, companyName, applicationName):
         self.__settings = QSettings(companyName, applicationName)
 
+    def clear(self):
+        self.__settings.clear()
+
     # API
     def get_selected_language(self):
         return self.__settings.value("Simply-Paint-Settings/selected_language")
 
     def set_selected_language(self, lang):
         self.__settings.setValue("Simply-Paint-Settings/selected_language", lang)
+
+
+if __name__=='__main__':
+    s = Settings("MySoft", "Simply Paint")
+    s.clear()
